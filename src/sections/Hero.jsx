022602a82ddb0d1fb3fe78244d29c81d26134fbd
Suryansh Singh <80690023+suryanshsingh2001@users.noreply.github.com>
2023-09-06 00:17:53 +0530

    Screenshots

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -25,19 +25,18 @@ const Hero = () => {
     >
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28">
         <p className="text-xl font-montserrat text-coral-red">
-          Our Summer collections
+        Summer Collection 2023
         </p>
 
         <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
           <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
-            The New Arrival
+          Latest Arrivals
           </span>
           <br />
           <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
         </h1>
         <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
-          Discover stylish Nike arrivals, quality comfort, and innovation for
-          your active life.
+        Explore the Latest Nike Innovations for Your Active Lifestyle.
         </p>
 
         <Button label="Shop now" iconURL={arrowRight} />
